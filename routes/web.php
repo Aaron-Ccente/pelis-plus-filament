95a426ac3803f6005e894df56d4ffa4129acdcbb
Aaron-Ccente <aronccente@gmail.com>
2026-02-25 19:19:43 -0500

    feat: controladores movidos a api.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\ActorController;
-use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/admin/login');
 });
-
-Route::get('api/movies', [MovieController::class, 'getAllmoviesAPI']);
-Route::get('api/actors', [ActorController::class, 'getActors']);
